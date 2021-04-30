8b17c4580f93a9b3de9ac4e949b85840eb6f9524
Luis Gómez <luisgomezsahun@gmail.com>
2021-04-30 23:06:16 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Luis Gomez on 30/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
